45a05f37c93f9ce6bcd885c09e5aa4834022abb5
Htk <143769464+aaronjimenez18@users.noreply.github.com>
2023-12-08 15:13:00 -0600

    Add files via upload
    creacion de la funcion burbujazo, la cual nos ayuda para el ordenamiento de las palabras mediante el codigo ascii y elimina las palabras repetidas

--- PROYECTOFINAL.cpp
+++ PROYECTOFINAL.cpp
@@ -15,6 +15,51 @@
 #include "corrector.h"
 #define DEPURAR 0
 
+
+void Burbujazo(char arr[][TAMTOKEN], int iEstadisticas[], int* n) {
+	for (int i = 0; i < (*n) - 1; ++i) {
+		for (int j = 0; j < (*n) - i - 1; ++j) {
+			// Comparar palabras e intercambiar
+			if (strcmp(arr[j], arr[j + 1]) > 0) {
+				// Intercambiar palabras
+				char temp[TAMTOKEN];
+				strcpy_s(temp, arr[j]);
+				strcpy_s(arr[j], arr[j + 1]);
+				strcpy_s(arr[j + 1], temp);
+			}
+		}
+	}
+
+	// Eliminar palabras duplicadas
+	int newSize = 0;
+	for (int i = 0; i < *n; ++i) {
+		if (i == 0 || strcmp(arr[i], arr[i - 1]) != 0) {
+			// Copiar palabras no repetidas
+			strcpy_s(arr[newSize], arr[i]);
+			newSize++;
+
+
+			int contador = 1;
+			for (int j = i + 1; j < *n; ++j) {
+				if (strcmp(arr[i], arr[j]) == 0) {
+					// La palabra actual es igual a la siguiente
+					contador++;
+				}
+				else {
+					// La palabra actual es diferente a la siguiente
+					iEstadisticas[newSize - 1] = contador;
+					break;
+				}
+			}
+		}
+	}
+
+
+	*n = newSize;
+}
+
+
+
 //Funciones publicas del proyecto
 /*****************************************************************************************************************
 	DICCIONARIO: Esta funcion crea el diccionario completo
@@ -83,6 +128,11 @@ void	Diccionario			(char *szNombre, char szPalabras[][TAMTOKEN], int iEstadistic
 			}
 			if (DEPURAR == 1)
 				printf("\nNumPalabras: %i\n", iNumElementos);
+                         
+
+// burbujazo
+			Burbujazo(szPalabras, iEstadisticas, &iNumElementos);
+
 
 			
 
